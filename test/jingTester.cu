28164cef867c6f96bc15522dc7dcc43b8e59dd11
jinghe <violet.hj@gmail.com>
2015-04-05 19:01:21 -0400

    PASS small scale test for gene permutation in CPU

--- test/jingTester.cu
+++ test/jingTester.cu
@@ -1,34 +1,28 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include "Matrix.hpp"
+//#include "Matrix.hpp"
+//#include "genMiCutoff.hpp"
 #include "genMiCutoff.hpp"
-#include "genMiCutoff.h"
+#include "genMiCutoff.cpp"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
+    if (argc != 3) {
         perror("sample number and permutation number!");
         exit(1);
     }
-    int nSamples = atoi(argv[1]);
-    int nperm = atoi(argv[2]); // this will change to fix value after testing  
+    const int Nsmp = atoi(argv[1]);
+    const int Nperm = atoi(argv[2]); // this will change to fix value after testing  
 
     srand(time(NULL));
 
-    Matrix<float> *h_mat = new Matrix<float>(nGenes, nSamples);
-    for (int i = 1; i < nGenes; i++){
-        for (int j = 0; j < nSamples; j++){
-            h_mat->element(i,j) = (rand() % 100) / 20.0;
-        }
-    }
-    h_mat->print();
-
-    cout << "start calculating null model mi" << endl;
+    cout << "start calculating null model mi .... " << endl;
     clock_t start = clock();
 
-    float h_micut = getRankMatrix(10,2); 
+    float h_micut;
+    h_micut  = calMIcutCoeff( Nsmp, Nperm ); 
 
     clock_t stop = clock();
     
