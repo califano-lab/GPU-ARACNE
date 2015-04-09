a559843d5b6566d52bcbaf2ece3c4f261532c0dc
jinghe <violet.hj@gmail.com>
2015-04-09 17:24:52 -0400

    delete unnecessary function

--- src/genMiCutoff.cu
+++ src/genMiCutoff.cu
@@ -25,7 +25,7 @@ void genMIcutoff( float *dMat, float pValue, float *miCutoff )
   int subSmp[ NsubSmp ]; 
   for ( int i = 1; i < Nsubsmp; ++i )
   {
-    subSmp[i] = (int) (0.3 + (i - 1) * 0.05) * Nsmp  ; 
+    subSmp[i-1] = (int) (0.3 + (i - 1) * 0.05) * Nsmp  ; 
   }
 
   const int numRep = 3;
@@ -38,20 +38,38 @@ void genMIcutoff( float *dMat, float pValue, float *miCutoff )
   float *beta_h;
   *beta_h = (float *) malloc( sizeof(float) * NsubSmp * numRep ) ; 
 
-  // copy data, alpha_h, beta_h to device 
-
   // add some random nosie to data  kernel, leave data on kernel 
 
+  // copy alpha_h, beta_h to device 
+  // do permutation for each subsampling 
+  float *dmat_sub_d;
+  cudamalloc( *dmat_sub_d, sizeof(dMat_h) ) ; // use the maximum size   
+
   // for each sub sample and each replicate calculate threshold and coefficient 
-  extraplotaMICutoff_d <<< >>> ( *dMat, Nperm )  
-  
-  // copy data back 
- 
+  // run for 14 * 3 times 
+  for ( int isubSmp = 0; isubSmp < NsubSmp ; ++isubSmp ) {
+     for ( int inumRep  = 0; inumRep < numRep ; ++ inumRep ) {
+        // take a subset of dMat,  
+        // and generate a similiar size of array with random permuated integer 
+        genRandIntArray_h( *dmat_sub_h, *subSmp[isubSmp] ) ; 
+
+        // overload the dmat_sub_d pointer every time launch a size >= previous_size
+        cudaMemcpy( dmat_sub_d, dmat_sub_h, 0, Host2Device );      
+
+        // launch kernel 
+	extraplotaMICutoff_d <<< ( 1,1,1), (1,1,1) >>> ( *dmat_sub_d, Nperm, *coef_d ); 
+
+	// copy result back
+	__cudasyncDevice(); 
+	cudaMemcpy( coef_h, coef_d, Device2Host) ;
+   }
+  }
+
   // linear fitting 
     float coef[2]; 
+    coef = subSmp ~ mean(beta_h) ;  // linear regression 
 
-    coef = subSmp ~ mean(beta_h) ; 
-    float a = mean( alpha_h) ; // overall alpha 
+    float a = mean( alpha_h ) ; // overall alpha 
     float b = coef[0]; 
     float c = coef[1]; 
 
@@ -62,120 +80,27 @@ void genMIcutoff( float *dMat, float pValue, float *miCutoff )
 
 }
 
-__global__ 
-void extrapolateMICutoff_d( float *dMat, const int Nperm ) 
+// local function 
+void extrapolateMICutoff_d( float *dMat, const int Nperm, float *nullMI ) 
 {
+  // input dMat is random permuted integers with Nsmp_sub <= Nsmp Columns 
+  // and Ngene rows;
+  const int Ngene = dMat.getNumRows() ;
+  const int Nsmp = dMat.getNumColumns() ;
+  // random take 2 index 
 
-}
+  int randomI; 
+  int randomJ; 
 
-int* genRandIntArray ( int array[], const int N )
-{
- // init random seed 
-   // struct timeval tv;
-   // gettimeofday( &tv, NULL);
-   // int usec = tv.tv_usec ; 
-   // srand(usec);
+  // prepare random datadata 
+  X = dMat[ randomI ]; 
+  Y = dMat[ randomJ ];
 
-   printf("Total: %d\n", N);
-     
-   int *a ;
-   a = ( int * ) malloc( N * sizeof( int ) ) ;
-   for ( int i=0; i< N; ++i)
-   {
-     a[i] = i + 1;
-   }
 
-   for ( int i = N - 1; i > 0; --i)
-   {
-     size_t j = (unsigned int ) ( drand48() * (i+ 1));
-     int t = a[j] ; 
-     a[j] = a[i]; 
-     a[i] = t; 
-   }
-   for ( int i = 0; i < N; ++i ) 
-   {
-     printf("%d\t",  a[i]) ;
-   }
-   printf( "\n");
-   return a;
-}
-
-float calMIcutoff ( float * mi, const int  NTOPPERCENT, const float pCut )
-{
-  float coeff = [2];
-  return coeff;
 }
 
 __global__
-float calMIap_d (int *X, int *Y, const int N){
-  //
-  
- if ( gridIdx.x > N | gridIdx.y > N) return; 
-
- // init shared mem;
-   
-}
-
-
-float calNullMI (const int Nsmp, const int Nperm) {
-    // use miAP kernel to calculate null model mi 
-    // return a float array of Nperm  
-    // generate  permutation arrays
-    int *h_X;
-    h_X = (int * ) malloc( sizeof (int) * Nsmp);
-    h_X = genRandIntArray( h_X, Nsmp );
-
-    int *h_Y;
-    h_Y = (int * ) malloc( sizeof (int) * Nsmp);
-    h_Y = genRandIntArray( h_Y, Nsmp );
-   
-    //    // Grid size and block Size
-    //    int numBlockx = 1; // number of pairs 
-    //    int numBlocky = 1;
-    //
-    //    int tileSize = 5;
-    //    int numThreadXPerBlock = Nsmp / tileSize ; 
-    //    int numThreadYPerBlock = Nsmp / tileSize ; 
-    //
-    //    // allocate memory, and copy the data over:
-    //
-    //    int *h_mi;  
-    //    h_mi = (float *) malloc( sizeof( float) * numBlockx * numBlocky);  
-    //    
-    //    int *d_X,  *d_Y;
-    //    int *d_mi;
-    //     
-    //    GPU_CHECKERROR( cudaMalloc ((void **) &d_X, Nsmp * sizeof (int)) );
-    //    GPU_CHECKERROR( cudaMalloc ((void **) &d_Y, Nsmp * sizeof (int)) );
-    //      
-    //    GPU_CHECKERROR( cudaMemcpy(d_X, h_X, Nsmp * sizeof(int), cudaMemcpyHostToDevice) ) ;
-    //    GPU_CHECKERROR( cudaMemcpy(d_Y, h_Y, Nsmp * sizeof(int), cudaMemcpyHostToDevice) ) ;
-    //        
-    //    // launch the kernel:
-    //
-    //    calMIap_d <<< (numBlockx, numBlocky, 1), (numThreadXPerBlock, numThreadYPerBlock, 1) >>> ( float * d_mi, int * d_X, int * d_Y, int Nsmp);
-    //    
-    //
-    //    // copy results back
-    //    GPU_CHECKERROR( cudaDeviceSynchronize() );
-    //    GPU_CHECKERROR( cudaMemcpy(h_mi,d_mi, numBlockx * numBlocky * sizeof(float), cudaMemcpyDeviceToHost) ) ;
-    //
-    //    // Postprocessing to generate cutoff
-    //    free(h_X); 
-    //    free(h_Y); 
-    //    free(h_mi); 
-    //    cudafree(d_X);
-    //    cudafree(d_Y);
-    //    cudafree(d_mi);
-
-    return 0.0;
-}
-
-void extrapolateMICutoff( float *matrix, const int Nsmp, float *coef )
-{
-}
-__global__
-void calCutoff ( float *nullMIArray , float *coef, const int Nperm, float pPvalue ) 
+void genCoef_subSmp ( float *nullMIArray , float *coef, const int Nperm, float pPvalue ) 
 {
   // run use one block 
   // sort 
@@ -220,5 +145,31 @@ void calCutoff ( float *nullMIArray , float *coef, const int Nperm, float pPvalu
   
 }
 
-void calCoeff ( 
+void genRandIntArray_h ( int *a, const int N )
+{
+   // helper function to generate random permuation of N samples init random seed 
 
+   printf("Total: %d\n", N);
+     
+   // int *a ;
+   // a = ( int * ) malloc( N * sizeof( int ) ) ;
+   for ( int i=0; i< N; ++i)
+   {
+     a[i] = i + 1;
+   }
+
+   for ( int i = N - 1; i > 0; --i)
+   {
+     size_t j = (unsigned int ) ( drand48() * (i+ 1));
+     int t = a[j] ; 
+     a[j] = a[i]; 
+     a[i] = t; 
+   }
+   // for ( int i = 0; i < N; ++i ) 
+   //{
+    // printf("%d\t",  a[i]) ;
+  // }
+   //printf( "\n");
+   //return a;
+   return;
+}
