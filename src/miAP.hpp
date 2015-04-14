20454980dcaedf24014c03f5e2efa88cf1313d7e
Zhou Zhou <zz2181@columbia.edu>
2015-04-14 00:27:24 -0400

    miAP, half the way, very hard to write

--- src/miAP.hpp
+++ src/miAP.hpp
@@ -5,6 +5,8 @@
 #include <cstdio>
 #include <cstdlib>
 
+
+
 template <typename T>
 __global__ 
 void computeMi(T *d_rankMatrix, unsigned int nTFs, unsigned int nGenes, unsigned int nSamples, 
@@ -13,19 +15,58 @@ void computeMi(T *d_rankMatrix, unsigned int nTFs, unsigned int nGenes, unsigned
     unsigned int TFIdx = blockIdx.x;
     unsigned int geneIdx = blockIdx.y;
     // need pick two rows of the d_rankMatrix
-    // first row: d_TFGenesIdx[TFIdx]
-    // second row: geneIdx
+    // first row (define it X): d_TFGenesIdx[TFIdx]
+    // second row (define it Y): geneIdx
     __shared__ Cube *cubeArray = new Cube[nSamples / 4 + 1];
     unsigned int head = 0;
-    unsigned int tail = 0;
+    unsigned int tail = 1;
     Cube cube;
     cube.totalCount = nSamples;
-    cube.upper = ......................
-    dim3 blockDim(256, 1, 1);
-    dim3 gridDim(ceil(nSamples / 256.0), 1, 1);
-    // launch another kernel here
+    cube.upper = round(nSamples - 1);
+    cube.lower = 0;
+    cube.left = 0;
+    cube.right = round(nSamples - 1);
+    cubeArray[head] = cube;
+    __shared__ unsigned int upperRight = 0;
+    __shared__ unsigned int upperLeft = 0;
+    __shared__ unsigned int lowerLeft = 0 ; 
+    __shared__ unsigned int lowerRight = 0;
+    do{
+        unsigned int colIdx = threadIdx.x;
+        unsigned int middleX = (cubeArray[head].left + cubeArray[head].right) / 2;
+        unsigned int middleY = (cubeArray[head].upper + cubeArray[head].lower) / 2;
+        unsigned int coordX = round(d_rankMatrix[d_TFGenesIdx[TFIdx] * nSamples + colIdx]);
+        unsigned int coordY = round(d_rankMatrix[geneIdx * nSamples + colIdx]);
+        
+        if (cubeArray[head].left <= coordX && coordX < middleX){
+            if (cubeArray[head].lower <= coordY && coordY < middleY){
+                atomicAdd(&lowerLeft, 1);
+            } else {
+                atomicAdd(&upperLeft, 1);
+            }
+        } else {
+            if (cubeArray[head].lower <= coordY && coordY < middleY){
+                atomicAdd(&lowerRight, 1);
+            } else {
+                atomicAdd(&upperRight, 1);
+            }
+        }
+        __syncthreads();
 
-
+        // designate one thread to compute this how to optimize?
+        if (threadIdx.x == 0){
+            if (chiSeq(upperRight, upperLeft, lowerLeft, lowerRight)) {// significant
+                cubeArray[tail].totalCount = upperRight;
+                cubeArray[tail].upper 
+                    ........ ...........
+                cubeArray[tail + 1].totalCount = upperLeft;
+                cubeArray[tail + 2].totalCount = lowerLeft;
+                cubeArray[tail + 3].totalCount = lowerRight;
+                tail = tail + 4;
+                head = head + 1;            
+            }
+        }
+    } while(head < tail);
 }
 
 
@@ -37,7 +78,7 @@ void miAP(T *d_rankMatrix, unsigned int nTFs, unsigned int nGenes, unsigned int 
         unsigned int *d_TFGeneIdx, T **d_rawGraph)
 {
     dim3 gridDim(nTFs, nGenes, 1);
-    dim3 blockDim(1, 1, 1);
+    dim3 blockDim(nSamples, 1, 1);
 
     HANDLE_ERROR( cudaMalloc((void **)d_rawGraph, sizeof(T) * nTFs * nGenes) );
     computeMi<<<blockDim, gridDim>>>(d_rankMatrix, nTFs, nGenes, nSamples, d_TFGeneIdx, d_mi);
